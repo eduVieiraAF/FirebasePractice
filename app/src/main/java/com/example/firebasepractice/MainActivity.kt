bd3f4c1da5d62c6d59322c4d38ab308b373450b3
eduVieiraAF <edu_vieira@hotmail.com>
2021-12-30 23:31:49 -0300

    Removing import I decided not to use in this example

--- app/src/main/java/com/example/firebasepractice/MainActivity.kt
+++ app/src/main/java/com/example/firebasepractice/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.firebasepractice
 
-import android.app.Dialog
-import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import com.example.firebasepractice.databinding.ActivityMainBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
