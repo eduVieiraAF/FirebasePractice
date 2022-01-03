575712bf72e056a5f4a265a058e7740e286838a3
eduVieiraAF <edu_vieira@hotmail.com>
2022-01-03 11:12:03 -0300

    A few elegant changes

--- app/src/main/java/com/example/firebasepractice/MainActivity.kt
+++ app/src/main/java/com/example/firebasepractice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.firebasepractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.firebasepractice.databinding.ActivityMainBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -12,7 +13,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var database: DatabaseReference
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -20,7 +20,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.txtName.requestFocus()
-
         binding.btnLogin.setOnClickListener {
 
             val fulName = binding.txtName.text.toString()
@@ -29,10 +28,15 @@ class MainActivity : AppCompatActivity() {
 
             if (fulName.isEmpty() || age.isEmpty() || userName.isEmpty()) {
 
-                Toast.makeText(
-                    this, "You cannot leave any field empty.", Toast
-                        .LENGTH_SHORT
-                ).show()
+                //Toast.makeText(this, "You cannot leave any field empty.", Toast
+                //    .LENGTH_SHORT).show()
+
+                AlertDialog.Builder(this)
+                    .setTitle("REGISTRATION")
+                    .setMessage("You cannot leave any field empty.")
+                    .setPositiveButton("Got it") {_,_ ->
+                        binding.txtName.requestFocus()}.show()
+
             }
 
             else {
@@ -43,8 +47,15 @@ class MainActivity : AppCompatActivity() {
 
                     binding.txtUserName.text.clear()
                     binding.txtUserName.requestFocus()
-                    Toast.makeText(this, "Username must not contain '.', '#', '$', '['," +
-                            " or ']'", Toast.LENGTH_LONG).show()
+
+                    //Toast.makeText(this, "Username must not contain '.', '#', '$', '['," +
+                    //        " or ']'", Toast.LENGTH_LONG).show()
+
+                    AlertDialog.Builder(this)
+                        .setTitle("USERNAME")
+                        .setMessage("Username cannot contain ., #, $, [, or ]")
+                        .setPositiveButton("Got it"){_,_ ->}
+                        .show()
                 }
 
                 else {
@@ -58,14 +69,32 @@ class MainActivity : AppCompatActivity() {
                         binding.txtUserName.text.clear()
                         binding.txtName.requestFocus()
 
-                       Toast.makeText(this, "Successfully registered", Toast.LENGTH_SHORT)
-                           .show()
+                       //Toast.makeText(this, "Successfully registered", Toast.LENGTH_SHORT)
+                        //   .show()
+                        AlertDialog.Builder(this)
+                            .setTitle("REGISTRATION")
+                            .setMessage("You have successfully completed\nyour registration.")
+                            .setPositiveButton("Okay"){_,_ ->}
+                            .show()
 
                     }.addOnFailureListener {
 
-                        Toast.makeText(this, "Registration failed", Toast.LENGTH_SHORT)
-                            .show()
+                        //Toast.makeText(this, "Registration failed", Toast.LENGTH_SHORT)
+                        //    .show()
 
+                        AlertDialog.Builder(this)
+                            .setTitle("REGISTRATION")
+                            .setMessage("Registration failed")
+                            .setNeutralButton("Try again"){_,_ ->
+
+                                binding.txtName.text.clear()
+                                binding.txtAge.text.clear()
+                                binding.txtUserName.text.clear()
+                                binding.txtName.requestFocus()
+                            }
+                            .setPositiveButton("Close app"){_,_ ->
+                                finish()}
+                            .show()
                     }
                 }
             }
